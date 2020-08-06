aeea0ff2c019f1e083b5c21a0725d71076f013cc
David Fuentes <davidf0827@gmail.com>
2020-08-06 07:26:32 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,20 @@
-# code here!
+class School
+  attr_reader :roster, :name
+  
+  def initialize(name)
+    @name = name
+    
+    @roster = {}
+  end
+  
+  def add_student(name, grade)
+    roster[grade] = []
+    roster[grade] << name
+  end
+  
+  def grade(grade)
+    roster.
+    
+    puts roster[grade]
+  end 
+end 
